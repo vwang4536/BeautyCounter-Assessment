2f6fbd8583713db4eef643e1174a29a7f77bbbb2
vwang4536 <vwang4536@gmail.com>
2020-08-08 18:41:50 -0700

    added server file, implemented start server script

---
+++ src/server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+require('dotenv').config();
+
+// Configure server
+const app = express();
+const port = 3000;
+
+// Server start
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
