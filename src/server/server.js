0b9f7423b38c3a85993d1b658142d11eca568e13
vwang4536 <vwang4536@gmail.com>
2020-08-09 00:26:30 -0700

    installed mongodb and mongoose, added connection to database within server file

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const fruitRoutes = require('./routes/fruitRoutes');
 require('dotenv').config();
 
@@ -12,6 +13,14 @@ app.use(fruitRoutes);
 // Server start
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
+});
+
+// Connect to database
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rbr6u.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, {
+  useNewUrlParser: true,
+});
+mongoose.connection.once('open', () => {
+  console.log('You are connected to the database');
 });
 
 module.exports = app;
