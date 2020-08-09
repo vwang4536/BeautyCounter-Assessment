4e9e6cbdeab82e1bdf4c2b2c4b78b1f07499297e
vwang4536 <vwang4536@gmail.com>
2020-08-08 18:47:22 -0700

    added foodRoutes file for all routes

---
+++ src/server/routes/foodRoutes.js
@@ -0,0 +1,4 @@
+const app = require('../server')
+
+// Routes: GET
+app.get('/food');
--- src/server/server.js
+++ src/server/server.js
@@ -10,3 +10,5 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
+
+module.exports = app;
